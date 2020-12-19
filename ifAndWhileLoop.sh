d63d564002fe2e9c46255d995fc7c1e2ee6e95c8
mazerlodge <mazerlodge@hotmail.com>
2020-12-19 11:56:03 -0600

    Updated if -z test to elaborate on meaning.

--- ifAndWhileLoop.sh
+++ ifAndWhileLoop.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
+# get baseline for checking sleep
+date
+
+# check for command having a parameter
 if [ ! -z $1 ] 
 then 
-    echo $1 
+    echo "test 1 - parameter was found in bang (last command) val = " $1 
 fi 
+
+# check for command having a parameter
+if [ -z $1 ] 
+then 
+    echo "test for param - parameter was not found " 
+fi 
+
 
 x=1
 
-while [ $x -lt 5 ] 
+while [ $x -lt 6 ] 
 do
     # bash syntax for variable assignment 
-    x=$((x + 1))
     echo Hi $x
+    x=$((x + 1))
     sleep 1
 done
+
+# check how long sleep took.
+date 
  
 
