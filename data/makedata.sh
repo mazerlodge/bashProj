cfd08b718fb640bfadff3aee61df1e60af36077a
mazerlodge <mazerlodge@hotmail.com>
2021-12-12 11:49:16 -0600

    Expanded loopThroughFiles to do datestamp checking

--- data/makedata.sh
+++ data/makedata.sh
@@ -2,4 +2,5 @@
 
 touch SourceSystem_Topic__20210501120009.txt 
 touch SourceSystem_Topic__20210502110021.txt 
+touch SourceSystem_Topic__20210501130042.txt
 touch SourceSystem_Topic__20210429142342.txt 
--- loopThroughFiles.sh
+++ loopThroughFiles.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 
-for FILE in ./data/*; 
+declare -i maxDatePart=0
+declare -i maxTimePart=0
+for currFile in ./data/*; 
 do
-	currFile=$FILE;
-	#echo $currFile; 
-
 	# SourceSystem_Topic__20210501120009.txt
-	filePrefix=${FILE:0:27}
-	echo $filePrefix
+	filePrefix=${currFile:0:27}
+	echo "Working on file with prefix $filePrefix"
 
 	# only check datestamp on files that match the target prefix
 	if [ "$filePrefix" = "./data/SourceSystem_Topic__" ]; 
 	then 
-		dateStamp=${FILE:27:14}
-		echo $dateStamp 
+		dateStamp=${currFile:27:14}
+		echo "Got dateStamp $dateStamp"
+
+		# capture date part and time part as numbers
+		declare -i datePart=${dateStamp:0:8}
+		declare -i timePart=${dateStamp:8:6}
+		echo "Dp $datePart Tp $timePart"
+
+		# compare to max values, replace if current file's datePart is bigger. 
+		if [ $datePart -gt $maxDatePart ];
+		then
+			maxDatePart=datePart
+			maxTimePart=timePart
+			echo "reset max vals to $maxDatePart and $maxTimePart"
+		elif [ $datePart -eq $maxDatePart -a $timePart -gt $maxTimePart ];
+		then
+			# if dates match and current time part is bigger replace max values.
+			maxDatePart=datePart
+			maxTimePart=timePart
+			echo "dates matched, reset max vals based on time to $maxDatePart and $maxTimePart"
+
+		fi
 
 	fi 
 
